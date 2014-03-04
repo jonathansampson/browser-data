3221d15c15a7a4f0f95113ff1a8ecb23e76db0d2
Jonathan Sampson <jjdsampson@gmail.com>
2014-03-04 15:53:35 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,75 @@
+require([
+    "data/chrome", 
+    "data/firefox",
+    "data/opera",
+    "data/internet explorer"
+], function( chrome, firefox, opera, ie ) {
+
+    var browsers = [ chrome, firefox, opera, ie ];
+    var properties = [];
+    
+    function copyUnique( source, destination ) {
+        source.forEach(function( value ) {
+            if ( destination.indexOf( value ) < 0 ) {
+                destination.push( value );
+            }
+        });
+    }
+
+    browsers.forEach(function( browser ){
+        copyUnique( browser, properties );
+    });
+    
+    properties.sort(function(a, b) {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        
+        return a < b ? -1 : a > b ? 1 : 0;
+    });
+    
+    var row, 
+        cell, 
+        table = document.createElement("table");
+    
+    /* Table Labels */
+    row = document.createElement("tr");
+    [ "Property", "Chrome", "Firefox", "Opera", "IE" ].forEach(function(value){
+        cell = document.createElement("th");
+        cell.appendChild( document.createTextNode( value ) );
+        row.appendChild(cell);
+    });
+    table.appendChild(row);
+    
+    /* Table Body */
+    properties.forEach(function( property ){
+        
+        row = document.createElement("tr");
+        
+        cell = document.createElement("td");
+        cell.appendChild(document.createTextNode(property));
+        row.appendChild(cell);
+        
+        browsers.forEach(function( browser ){
+            cell = document.createElement("td");
+            cell.innerHTML = browser.indexOf(property) < 0 ? "" : "&#10004;";
+            row.appendChild(cell);
+        });
+        
+        table.appendChild(row);
+        
+    });
+    
+    document.body.appendChild( table );
+    
+    document.querySelector("input").addEventListener("keyup", function () {
+        for ( var i = 1; i < table.rows.length; i++ ) {
+            var pattern = new RegExp(this.value, "i");
+            if ( table.rows[i].cells[0].textContent.match(pattern) ) {
+                table.rows[i].style.display = "";
+            } else {
+                table.rows[i].style.display = "none";
+            }
+        }
+    });
+
+});
